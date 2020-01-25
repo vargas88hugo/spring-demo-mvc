74177beec4ccebd710f686852eccbc3b32a7f8e1
Hugo Vargas <vargas88hugo@gmail.com>
2020-01-25 13:39:15 -0500

    [Feature] Implement request param to process the data in the model

--- src/com/springdemo/mvc/HelloWorldController.java
+++ src/com/springdemo/mvc/HelloWorldController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -32,6 +33,24 @@ public class HelloWorldController {
 		
 		// create the message
 		String result = "Yo! " + theName;
+		
+		// add message to the Model
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
+	
+	// Binding request params for processing the form and add to the model
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(
+			@RequestParam("studentName") String theName, 
+			Model model) {
+		
+		// convert the data to UperCase
+		theName = theName.toUpperCase();
+		
+		// create the message
+		String result = "Hey my friend from v3! " + theName;
 		
 		// add message to the Model
 		model.addAttribute("message", result);
