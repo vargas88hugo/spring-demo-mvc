97075a0b3cbf5274dc76a1e270936665a758bd9c
Hugo Vargas <vargas88hugo@gmail.com>
2020-01-25 17:50:23 -0500

    [Feature] Implemented form validation with a range of numbers

--- src/com/springdemo/mvc/Customer.java
+++ src/com/springdemo/mvc/Customer.java
@@ -1,5 +1,7 @@
 package com.springdemo.mvc;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -11,6 +13,16 @@ public class Customer {
 	@Size(min=1, message="is required")
 	private String lastName;
 	
+	@Min(value=0, message="must be greater than or aqual to zero")
+	@Max(value=10, message="must be less than or equal to ten")
+	private int freePasses;
+	
+	public int getFreePasses() {
+		return freePasses;
+	}
+	public void setFreePasses(int freePasses) {
+		this.freePasses = freePasses;
+	}
 	public String getFirstName() {
 		return firstName;
 	}
